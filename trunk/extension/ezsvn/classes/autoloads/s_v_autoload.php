88056249b341325cd1848a6a3ddbc2ab8a208094
Björn Dieding <bjoern@xrow.de>
2008-08-27 05:12:11 +0000

    moved exceptions
    git-svn-id: http://svn.projects.ez.no/ezsvn@22 b5d9bed5-a27a-4f52-b0b6-f1a43407f1cc

--- trunk/extension/ezsvn/classes/autoloads/s_v_autoload.php
+++ trunk/extension/ezsvn/classes/autoloads/s_v_autoload.php
@@ -2,6 +2,9 @@
 return array (
 	'xrowSVN' => 'SVN/xrowsvn.php',
 	'xrowSVNRecursiveDirectoryIterator' => 'SVN/xrowsvnrecursivedirectoryiterator.php',
-	'xrowSVNWorkingCopy' => 'SVN/xrowsvnworkingcopy.php'
+	'xrowSVNWorkingCopy' => 'SVN/xrowsvnworkingcopy.php',
+	'xrowSVNUpdateException' => 'SVN/exceptions/update.php',
+	'xrowSVNLockException' => 'SVN/exceptions/lock.php',
+	'xrowSVNDifferentPathException' => 'SVN/exceptions/differentpath.php'
 );
 ?>
